abf05b4160f58fc6ec373191d9cef940aa952c9b
luiscaballeroramos <260264168+cmluiscr@users.noreply.github.com>
2026-05-04 11:18:15 +0200

    update printdb: txt option

--- printdb.py
+++ printdb.py
@@ -1,14 +1,28 @@
 from register.db import SimpleDB
-from config import DBNAME, DBSCHEMA
+from config import DBNAME, DBSCHEMA_LISTENINGEVENT
 
-def print_db():
+def print_db(dbname=DBNAME, dbschema=DBSCHEMA_LISTENINGEVENT, output_file=None):
     # database
-    db = SimpleDB(DBNAME, DBSCHEMA)
+    db = SimpleDB(dbname, dbschema)
+    output = []
+
     # print database contents & number of rows in each table
-    for table in DBSCHEMA.keys():
-        db.print_table(table)
+    for table in dbschema.keys():
+        table_data = db.print_table(table)  # Assuming print_table can return data
         count = db.count_rows(table)
-        print(f"{count} rows in '{table}': \n")
+        output.append(f"{count} rows in '{table}':\n{table_data}\n")
+        for row in db.cursor.execute(f"SELECT * FROM {table} ORDER BY played_at DESC").fetchall():
+            output.append(f"{row}\n")
+
+    if output_file:
+        with open(output_file, "w") as f:
+            f.writelines(output)
+    else:
+        print("\n".join(output))
+
+    print(f"Database '{dbname}' contents in table {table}: {sum(db.count_rows(table) for table in dbschema.keys())} registers")
 
 if __name__ == "__main__":
-    print_db()
+    dbname="spotify.db"
+    output_file= dbname.replace(".db", "") + ".txt"
+    print_db(output_file=output_file)
