04bd9cf9dd41249ac6c8a45a1f5bdf1358462249
luiscaballeroramos <260264168+cmluiscr@users.noreply.github.com>
2026-05-20 15:02:19 +0200

    update album, artist, track

--- register/artist.py
+++ register/artist.py
@@ -20,6 +20,29 @@ class Artist:
         self.popularity = popularity
         self.start_year = start_year
 
+    def edit(self,
+             db: SimpleDB,
+             id,
+             name: Optional[str] = None,
+             end_year: Optional[int] = None,
+             followers: Optional[int] = None,
+             images: Optional[List[str]] = None,
+             popularity: Optional[int] = None,
+             start_year: Optional[int] = None):
+        # check id exist in db
+        if not db.exists("artists", id):
+            print(f"Artist with id {id} does not exist in db")
+        else:
+            db.edit("artists", id, {
+                "name": name if name is not None else self.name,
+                "end_year": end_year if end_year is not None else self.end_year,
+                "followers": followers if followers is not None else self.followers,
+                "images": json.dumps(images) if images is not None else json.dumps(self.images),
+                "popularity": popularity if popularity is not None else self.popularity,
+                "start_year": start_year if start_year is not None else self.start_year
+            }, print_columns=["id", "name", "followers", "popularity", "start_year", "end_year"])
+
+
     def save(self, db: SimpleDB, print_only_insert=False) -> bool:
         return db.insert("artists", {
             "id": self.id,
--- register/track.py
+++ register/track.py
@@ -48,6 +48,58 @@ class Track:
         self.time_signature = time_signature
         self.valence = valence
 
+    def edit(self,
+             db,
+             id,
+             name: Optional[str] = None,
+             duration_ms: Optional[int] = None,
+             album_id: Optional[str] = None,
+             album_track: Optional[int] = None,
+             artists_ids: Optional[List[str]] = None,
+             acousticness: Optional[float] = None,
+             danceability: Optional[float] = None,
+             energy: Optional[float] = None,
+             explicit: Optional[bool] = None,
+             genres: Optional[List[str]] = None,
+             images: Optional[List[str]] = None,
+             instrumentalness: Optional[float] = None,
+             key: Optional[int] = None,
+             liveness: Optional[float] = None,
+             loudness: Optional[float] = None,
+             mode: Optional[int] = None,
+             popularity: Optional[int] = None,
+             speechiness: Optional[float] = None,
+             tempo: Optional[float] = None,
+             time_signature: Optional[int] = None,
+             valence: Optional[float] = None):
+        # check id exist in db
+        if not db.exists("tracks", id):
+            print(f"Track with id {id} does not exist in db")
+        else:
+            db.edit("tracks", id, {
+                "name": name if name is not None else self.name,
+                "duration_ms": duration_ms if duration_ms is not None else self.duration_ms,
+                "album_id": album_id if album_id is not None else self.album_id,
+                "album_track": album_track if album_track is not None else self.album_track,
+                "artists_ids": json.dumps(artists_ids) if artists_ids is not None else json.dumps(self.artists_ids),
+                "acousticness": acousticness if acousticness is not None else self.acousticness,
+                "danceability": danceability if danceability is not None else self.danceability,
+                "energy": energy if energy is not None else self.energy,
+                "explicit": explicit if explicit is not None else self.explicit,
+                "genres": json.dumps(genres) if genres is not None else json.dumps(self.genres),
+                "images": json.dumps(images) if images is not None else json.dumps(self.images),
+                "instrumentalness": instrumentalness if instrumentalness is not None else self.instrumentalness,
+                "key": key if key is not None else self.key,
+                "liveness": liveness if liveness is not None else self.liveness,
+                "loudness": loudness if loudness is not None else self.loudness,
+                "mode": mode if mode is not None else self.mode,
+                "popularity": popularity if popularity is not None else self.popularity,
+                "speechiness": speechiness if speechiness is not None else self.speechiness,
+                "tempo": tempo if tempo is not None else self.tempo,
+                "time_signature": time_signature if time_signature is not None else self.time_signature,
+                "valence": valence if valence is not None else self.valence
+            }, print_columns=["id", "name", "duration_ms", "album_id", "artists_ids"])
+
     def save(self, db, print_only_insert=False):
         return db.insert("tracks", {
             "id": self.id,
