831789989e82867651c7e6494da09cd684c0897e
luiscaballeroramos <63744250+luiscaballeroramos@users.noreply.github.com>
2026-06-24 00:20:40 +0200

    update spotifyclient

--- spotifyapi/spotifyclient.py
+++ spotifyapi/spotifyclient.py
@@ -1,5 +1,4 @@
 import spotipy
-from ast import List
 
 from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, VERBOSE
 from register.artist import Artist
@@ -13,25 +12,42 @@ class SpotifyClient:
                 client_id=CLIENT_ID,
                 client_secret=CLIENT_SECRET,
                 redirect_uri=REDIRECT_URI,
-                scope="user-read-playback-state user-read-recently-played",
+                scope="user-read-playback-state user-read-recently-played user-library-read",
             )
         )
 
     def get_artist_byid(self, artist_id: str) -> Artist:
         artist = self.sp.artist(artist_id)
-        images = [img['url'] for img in artist['images']] if 'images' in artist else []
-        return Artist(id=artist_id, name=artist['name'], images=images)
+        images = [img["url"] for img in artist["images"]] if "images" in artist else []
+        return Artist(id=artist_id, name=artist["name"], images=images)
 
     def get_artists_bytrackid(self, track_id: str):
         track = self.sp.track(track_id)
         artists = []
         # Adjusted to handle the structure of the Spotify API response
-        if 'artists' in track:  # Removed 'track' key assumption
-            for artist in track['artists']:
-                artist_info = self.sp.artist(artist['id'])
-                images=[img['url'] for img in artist_info['images']] if 'images' in artist_info else []
-                artists.append(Artist(id=artist['id'], name=artist_info['name'], images=images))
+        if "artists" in track:  # Removed 'track' key assumption
+            for artist in track["artists"]:
+                artist_info = self.sp.artist(artist["id"])
+                images = (
+                    [img["url"] for img in artist_info["images"]]
+                    if "images" in artist_info
+                    else []
+                )
+                artists.append(
+                    Artist(id=artist["id"], name=artist_info["name"], images=images)
+                )
         return artists
+
+    def get_track_byid(self, track_id: str):
+        try:
+            return self.sp.track(track_id)
+        except Exception:
+            (
+                print(f"Error in SpotifyClient.get_track_byid for {track_id}")
+                if VERBOSE
+                else None
+            )
+            return None
 
     def get_currently_playing(self):
         try:
@@ -40,7 +56,6 @@ class SpotifyClient:
             print("Error in SpotifyClient.get_currently_playing") if VERBOSE else None
             return None
 
-
     def get_recently_played(self, limit=20):
         try:
             return self.sp.current_user_recently_played(limit=limit)
@@ -48,4 +63,9 @@ class SpotifyClient:
             print("Error in SpotifyClient.get_recently_played") if VERBOSE else None
             return None
 
-
+    def get_liked_songs(self, limit=20, offset=0):
+        try:
+            return self.sp.current_user_saved_tracks(limit=limit, offset=offset)
+        except Exception:
+            print("Error in SpotifyClient.get_liked_songs") if VERBOSE else None
+            return None
