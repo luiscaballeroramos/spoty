b7bc1cd3fd289bbf20ddadc79006e5aaf28f546e
luiscaballeroramos <260264168+cmluiscr@users.noreply.github.com>
2026-08-21 17:58:02 +0200

    refactor: remove commented-out code and clean up register_listeningevents function

--- _registration.py
+++ _registration.py
@@ -146,7 +146,6 @@ def save_liked_track_ids(limit=50, offset=0):
 def register_listeningevents():
     spotify = SpotifyClient()
     db = SimpleDB(DBNAME)
-<<<<<<< HEAD
     print("Starting Spotify tracker (single run)...")
     (
         print(
@@ -231,96 +230,6 @@ def register_listeningevents():
     # get missing metadata for tracks in listening_events that are not in tracks table
     # get_missing_track_metadata(db, spotify, limit=1)
     print("Single run completed.")
-=======
-    offset_liked_tracks = 0
-    print("Starting Spotify tracker...")
-    # loop to get currently playing every 10 minutes and save to db
-    try:
-        while True:
-            (
-                print(
-                    f"---------------------------------\nFetching recently played tracks at {datetime.now()}"
-                )
-                if VERBOSE
-                else None
-            )
-            recently_played = spotify.get_recently_played(limit=50)
-            if recently_played and "items" in recently_played:
-                for item in recently_played["items"]:
-                    # track
-                    track = item["track"]
-                    track_id = track["id"]
-                    track_name = track["name"]
-                    track_duration_ms = track["duration_ms"]
-                    track_album_id = track["album"]["id"]
-                    track_album_track = track["track_number"]
-                    track_artists_ids = [artist["id"] for artist in track["artists"]]
-                    track_explicit = track["explicit"]
-                    _track = Track(
-                        id=track_id,
-                        name=track_name,
-                        duration_ms=track_duration_ms,
-                        album_id=track_album_id,
-                        album_track=track_album_track,
-                        artists_ids=track_artists_ids,
-                        explicit=track_explicit,
-                    )
-                    # print(f'Save TRACK: {track_name} ({track_id})') if VERBOSE else None
-                    _track.save(db, print_only_insert=True)
-                    # album
-                    album = track["album"]
-                    album_id = album["id"]
-                    album_name = album["name"]
-                    album_artists_ids = [artist["id"] for artist in album["artists"]]
-                    album_total_tracks = album["total_tracks"]
-                    album_images = (
-                        [img["url"] for img in album["images"]]
-                        if "images" in album
-                        else []
-                    )
-                    album_release_date = album["release_date"]
-                    album_release_date_precision = album["release_date_precision"]
-                    _album = Album(
-                        id=album_id,
-                        name=album_name,
-                        artists=album_artists_ids,
-                        tracks=[track_id],  # only save the current track, update later
-                        total_tracks=album_total_tracks,
-                        images=album_images,
-                        release_date=album_release_date,
-                        release_date_precision=album_release_date_precision,
-                    )
-                    # print(f'Save ALBUM: {album_name} ({album_id}) of year {_album.release_year}') if VERBOSE else None
-                    _album.save(db, print_only_insert=True)
-                    # artists
-                    artists = []
-                    for artist in track["artists"] + album["artists"]:
-                        artist_id = artist["id"]
-                        artist_name = artist["name"]
-                        _artist = Artist(id=artist_id, name=artist_name)
-                        artists.append(_artist)
-                        # print(f'Save ARTIST: {artist_name} ({artist_id})') if VERBOSE else None
-                        _artist.save(db, print_only_insert=True)
-                    # event
-                    date = datetime.fromisoformat(
-                        item["played_at"].replace("Z", UTC_OFFSET)
-                    )
-                    context_uri = item["context"]["uri"] if item["context"] else None
-                    event = ListeningEvent(
-                        track_id=track_id, played_at=date, context_uri=context_uri
-                    )
-                    # print(f'Save LISTENING EVENT: {track_name} at {date}') if VERBOSE else None
-                    event.save(db, print_only_insert=True)
-            # get missing metadata for tracks in listening_events that are not in tracks table
-            get_missing_track_metadata(db, spotify, limit=1)
-            # # save 50 liked track ids
-            # save_liked_track_ids(limit=50, offset=offset_liked_tracks)
-            # offset_liked_tracks += 50
-            # wait 10 minutes
-            time.sleep(600 / 2)
-    except KeyboardInterrupt:
-        print("Spotify tracker stopped by user.")
->>>>>>> db5b2a30ee787096e29d77378a5f09fb2dae6709
 
 
 if __name__ == "__main__":
